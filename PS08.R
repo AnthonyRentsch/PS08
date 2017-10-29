719938f41f6c4f0ed74b092b8a26c44b3c2d5115
Anthony Rentsch <anthony.rentsch@gmail.com>
2017-10-28 21:15:11 -0400

    Pushing PS work. Should be done, but may still revise.

--- PS08.R
+++ PS08.R
@@ -5,17 +5,6 @@ library(caret)
 library(tictoc)
 
 
-
-# Demo of timer function --------------------------------------------------
-# Run the next 5 lines at once
-tic()
-Sys.sleep(3)
-timer_info <- toc()
-runtime <- timer_info$toc - timer_info$tic
-runtime
-
-
-
 # Get data ----------------------------------------------------------------
 # Accelerometer Biometric Competition Kaggle competition data
 # https://www.kaggle.com/c/accelerometer-biometric-competition/data
@@ -29,41 +18,86 @@ dim(train)
 # knn modeling ------------------------------------------------------------
 model_formula <- as.formula(Device ~ X + Y + Z)
 
-# Values to use:
-n_values <- c(10, 20, 30)
-k_values <- c(2, 3, 4)
-
-runtime_dataframe <- expand.grid(n_values, k_values) %>%
-  as_tibble() %>%
-  rename(n=Var1, k=Var2) %>%
-  mutate(runtime = n*k)
-runtime_dataframe
-
-
 
 
 # Time knn here -----------------------------------------------------------
+# I sample uniformly from all possible values of k and n. 
 
+n <- c(3e+06, 6e+06, 9e+06, 12e+06, 15e+06, 18e+06, 21e+06, 24e+06, 27e+06, nrow(train))
+k <- n
 
+runtime <- expand.grid(n, k) %>%
+  as_tibble() %>%
+  rename(n = Var1, k = Var2)
 
+time <- c()
+m <- 1
+i <- 1
+
+while(i <= length(n)){
+  data <- train[1:n[i],]
+  j <- 1
+  while(j <= length(k)){
+    tic()
+    caret::knn3(model_formula, data = data, k = k[j])
+    timer_info <- toc()
+    time[m] <- timer_info$toc - timer_info$tic
+    m = m + 1
+    j = j + 1
+  }
+  i = i + 1
+}
+  
+runtime <- cbind(runtime, time)
 
 # Plot your results ---------------------------------------------------------
-# Think of creative ways to improve this barebones plot. Note: you don't have to
-# necessarily use geom_point
-runtime_plot <- ggplot(runtime_dataframe, aes(x=n, y=k, col=runtime)) +
-  geom_point()
 
-runtime_plot
-ggsave(filename="firstname_lastname.png", width=16, height = 9)
+#k
+runtime_plot_k <- ggplot(runtime, aes(x = k, y = time, col = n)) +
+  geom_jitter() + geom_smooth(colour = "black", alpha = 0.5, se = FALSE) +
+  labs(title = "Number of nearest neighbors considered vs runtime", y = "runtime (seconds)")
+runtime_plot_k
 
+ggsave(filename="anthony_rentsch_k.png", width=16, height = 9)
 
+#n
+runtime_plot_n <- ggplot(runtime, aes(x = n, y = time, col = k)) +
+  geom_jitter() + geom_smooth(aes(group = k, col = k), alpha = 0.5, se = FALSE) +
+  labs(title = "Size of training set vs runtime", subtitle = "Lines included for all values of k", y = "runtime (seconds)")
+runtime_plot_n
 
+ggsave(filename="anthony_rentsch_n.png", width=16, height = 9)
+
+#d
+runtime_plot_d <- ggplot(runtime, aes(x = 3, y = time)) +
+  geom_point(aes(col = k)) + 
+  labs(title = "Number of predictors vs runtime", y = "runtime (seconds)", x = "Number of predictors")
+runtime_plot_d
+
+ggsave(filename="anthony_rentsch_d.png", width=16, height = 9)
 
 # Runtime complexity ------------------------------------------------------
 # Can you write out the rough Big-O runtime algorithmic complexity as a function
 # of:
-# -n: number of points in training set
-# -k: number of neighbors to consider
-# -d: number of predictors used? In this case d is fixed at 3
+
+# n: number of points in training set
+# Answer: O(c)
+# The runtime of the kNN model does not increase as the size of training set increases, 
+# as long as the parameter k is held still.
+
+
+# k: number of neighbors to consider
+# Answer: O(k^2)
+# The runtime of the kNN model increases exponentially as the parameter k increases, 
+# meaning that the value of k has a big impact on the model's algorithmic complexity.  
+
+
+# d: number of predictors used? In this case d is fixed at 3
+# Answer: O(c)
+# Hard to tell since we only consider the 3 predictor case in this problem set, but it would
+# seem that the runtime does not increase as d increases, as the algorithmic complexity is determined
+# more by how many computations it is forced to do (i.e., how many k neighbors to compare to) rather
+# than the number of points n to train the model on or the number of predictors d to build the
+# predictor space. 
 
 
