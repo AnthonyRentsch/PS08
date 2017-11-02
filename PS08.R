a3cd5bddc5d956b31e1acd40e21a404dfa1bee9c
Anthony Rentsch <anthony.rentsch@gmail.com>
2017-11-01 23:06:06 -0400

    Making Andrew's revisions.

--- PS08.R
+++ PS08.R
@@ -21,14 +21,14 @@ model_formula <- as.formula(Device ~ X + Y + Z)
 
 
 # Time knn here -----------------------------------------------------------
-# I sample uniformly from all possible values of k and n. 
+# I sample uniformly on k exists on [1,20] and on n exists on [1,000,000, 10,000,000].
 
-n <- c(3e+06, 6e+06, 9e+06, 12e+06, 15e+06, 18e+06, 21e+06, 24e+06, 27e+06, nrow(train))
-k <- n
+n <- c(1e+06, 2e+06, 3e+06, 4e+06, 5e+06, 6e+06, 7e+06, 8e+06, 9e+06, 10e+6)
+k <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
 
-runtime <- expand.grid(n, k) %>%
+runtime <- expand.grid(k, n) %>%
   as_tibble() %>%
-  rename(n = Var1, k = Var2)
+  rename(k = Var1, n = Var2)
 
 time <- c()
 m <- 1
@@ -52,52 +52,23 @@ runtime <- cbind(runtime, time)
 
 # Plot your results ---------------------------------------------------------
 
-#k
-runtime_plot_k <- ggplot(runtime, aes(x = k, y = time, col = n)) +
-  geom_jitter() + geom_smooth(colour = "black", alpha = 0.5, se = FALSE) +
+runtime_plot <- ggplot(runtime, aes(x = n, y = time, col = k)) +
+  geom_point() + geom_smooth(colour = "black", alpha = 0.5, se = FALSE) +
   labs(title = "Number of nearest neighbors considered vs runtime", y = "runtime (seconds)")
-runtime_plot_k
+runtime_plot
 
-ggsave(filename="anthony_rentsch_k.png", width=16, height = 9)
+ggsave(filename="anthony_rentsch.png", width=16, height = 9)
 
-#n
-runtime_plot_n <- ggplot(runtime, aes(x = n, y = time, col = k)) +
-  geom_jitter() + geom_smooth(aes(group = k, col = k), alpha = 0.5, se = FALSE) +
-  labs(title = "Size of training set vs runtime", subtitle = "Lines included for all values of k", y = "runtime (seconds)")
-runtime_plot_n
-
-ggsave(filename="anthony_rentsch_n.png", width=16, height = 9)
-
-#d
-runtime_plot_d <- ggplot(runtime, aes(x = 3, y = time)) +
-  geom_point(aes(col = k)) + 
-  labs(title = "Number of predictors vs runtime", y = "runtime (seconds)", x = "Number of predictors")
-runtime_plot_d
-
-ggsave(filename="anthony_rentsch_d.png", width=16, height = 9)
 
 # Runtime complexity ------------------------------------------------------
 # Can you write out the rough Big-O runtime algorithmic complexity as a function
 # of:
 
 # n: number of points in training set
-# Answer: O(c)
-# The runtime of the kNN model does not increase as the size of training set increases, 
-# as long as the parameter k is held still.
-
-
 # k: number of neighbors to consider
-# Answer: O(k^2)
-# The runtime of the kNN model increases exponentially as the parameter k increases, 
-# meaning that the value of k has a big impact on the model's algorithmic complexity.  
-
-
 # d: number of predictors used? In this case d is fixed at 3
-# Answer: O(c)
-# Hard to tell since we only consider the 3 predictor case in this problem set, but it would
-# seem that the runtime does not increase as d increases, as the algorithmic complexity is determined
-# more by how many computations it is forced to do (i.e., how many k neighbors to compare to) rather
-# than the number of points n to train the model on or the number of predictors d to build the
-# predictor space. 
+
+# Answer:
+# f(n) = 0(d(n^2 + k))
 
 
